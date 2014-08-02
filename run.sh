f3a69e593cc422b9458fdb8ccca649a0c34d29cf
JeremyOT <jeremyot@gmail.com>
2014-08-02 12:37:16 -0700

    fix arg passthrough

--- run.sh
+++ run.sh
@@ -2,10 +2,10 @@
 
 case $1 in
   redis-cli)
-    /usr/lib/redis/src/redis-cli ${@:2}
+    /usr/lib/redis/src/redis-cli "${@:2}"
     ;;
   *)
     cd /var/redis/data
-    /usr/lib/redis/src/redis-server ${@:2}
+    /usr/lib/redis/src/redis-server "${@:2}"
     ;;
 esac
